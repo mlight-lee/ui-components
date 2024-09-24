0df5a92bd43aa490df93d23817f8a2af177db252
Mlight Lee <mlight.lee@outlook.com>
2024-09-24 21:05:46 +0800

    feat: replace type of property 'icon' in MlButtonData from string to Vue Component

--- packages/ui-components/src/index.ts
+++ packages/ui-components/src/index.ts
@@ -2,18 +2,18 @@ import MlCollapse from './components/MlCollapse.vue'
 import MlDropdown from './components/MlDropdown.vue'
 import MlLanguage from './components/MlLanguage.vue'
 import MlStatusBar from './components/MlStatusBar.vue'
-import MlToolbar from './components/MlToolbar.vue'
+import MlToolBar from './components/MlToolBar.vue'
 import MlToolPalette from './components/MlToolPalette.vue'
 export {
   MlCollapse,
   MlDropdown,
   MlLanguage,
   MlStatusBar,
-  MlToolbar,
+  MlToolBar,
   MlToolPalette
 }
 export type { MlDropdownMenuItem } from './components/MlDropdown.vue'
-export type { MlButtonData } from './components/MlToolbar.vue'
+export type { MlButtonData } from './components/MlToolBar.vue'
 
 // Optionally, export them as a plugin for Vue
 export default {
@@ -23,7 +23,7 @@ export default {
     app.component('MlDropdown', MlDropdown)
     app.component('MlLanguage', MlLanguage)
     app.component('MlStatusBar', MlStatusBar)
-    app.component('MlToolbar', MlToolbar)
+    app.component('MlToolbar', MlToolBar)
     app.component('MlToolPalette', MlToolPalette)
   }
 }
